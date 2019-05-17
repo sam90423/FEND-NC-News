f26299459fc1b80d660e300d911ad7f1386cda57
sam wood <samwood.5@outlook.com>
2019-05-17 12:21:21 +0100

    I have adjusted my error page to take into account wrong route in the url

--- nc-news/src/components/Error.jsx
+++ nc-news/src/components/Error.jsx
@@ -7,7 +7,7 @@ export default class Error extends React.Component {
     return (
       <div>
         <h1>Sorry something went wrong there</h1>
-        {this.props.location.state.displayErr ? (
+        {this.props.location.state ? (
           <p>{this.props.location.state.displayErr}</p>
         ) : (
           <p>Sorry wrong route</p>
