a8ba65325433538ee09048e0f63e59df32b0211b
sam wood <samwood.5@outlook.com>
2019-08-11 12:55:37 +0100

    Changed the topics links to have videos in components/TopicList.jsx and added styling in index.css

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -3,14 +3,63 @@ import { Link } from "@reach/router";
 
 export const TopicList = props => {
   return (
-    <div>
-      {props.topics.slice(0, 20).map(topic => {
+    <div className="topicCardsCon">
+      {props.topics.slice(0, 1).map(topic => {
         return (
-          <div key={topic.slug} className="articleL">
+          <div key={topic.slug}>
             <Link className="articles" to={`${topic.slug}`}>
               <h2>{topic.slug}</h2>
             </Link>
-            <hr className="articleList" />
+            <hr className="cardHr" />
+            <Link to={`${topic.slug}`}>
+              <video
+                src="Codes - 23355.mp4"
+                height="325"
+                width="500"
+                autoPlay
+                loop
+              />
+            </Link>
+          </div>
+        );
+      })}
+
+      {props.topics.slice(1, 2).map(topic => {
+        return (
+          <div key={topic.slug}>
+            <Link className="articles" to={`${topic.slug}`}>
+              <h2>{topic.slug}</h2>
+            </Link>
+            <hr className="cardHr" />
+            <Link to={`${topic.slug}`}>
+              <video
+                src="Football - 15734.mp4"
+                height="325"
+                width="500"
+                autoPlay
+                loop
+              />
+            </Link>
+          </div>
+        );
+      })}
+
+      {props.topics.slice(2, 3).map(topic => {
+        return (
+          <div key={topic.slug}>
+            <Link className="articles" to={`${topic.slug}`}>
+              <h2>{topic.slug}</h2>
+            </Link>
+            <hr className="cardHr" />
+            <Link to={`${topic.slug}`}>
+              <video
+                src="Food - 24999.mp4"
+                height="325"
+                width="500"
+                autoPlay
+                loop
+              />
+            </Link>
           </div>
         );
       })}
