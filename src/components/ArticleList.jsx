fd948ebafbd9648b7b46a5b9fce5a1bf2bb23d2b
sam wood <samwood.5@outlook.com>
2019-08-11 13:23:23 +0100

    Changed the article cards to make the whole card a link and so each line of text underlines individualy and is a link to the article in components/ArticleList.jsx also I made styling changes in index.css

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -7,26 +7,29 @@ export const ArticleList = props => {
       {props.articles.slice(0, 1).map(article => {
         return (
           <div key={`${article.article_id}`}>
-            <video
-              className="latestPostVideo"
-              src="Pexels Videos 2342561.mp4"
-              height="400"
-              width="500"
-              autoPlay
-              loop
-            />
+            <Link to={`${article.article_id}`}>
+              <video
+                className="latestPostVideo"
+                src="Pexels Videos 2342561.mp4"
+                height="400"
+                width="500"
+                autoPlay
+                loop
+              />
+            </Link>
 
             <ul className="latestPostText">
-              <h1>
-                <Link className="articles" to={`${article.article_id}`}>
-                  {article.title}
-                </Link>
-              </h1>
+              <Link className="articles" to={`${article.article_id}`}>
+                <h1 className="articles">{article.title}</h1>
+              </Link>
+
               <hr className="cardHr" />
-              <h3>By: {article.author}</h3>
-              <h3>Date created: {article.created_at}</h3>
-              <h3>Comment count: {article.comment_count}</h3>
-              <h3>Votes: {article.votes}</h3>
+              <h3 className="articles">By: {article.author}</h3>
+              <h3 className="articles">Date created: {article.created_at}</h3>
+              <h3 className="articles">
+                Comment count: {article.comment_count}
+              </h3>
+              <h3 className="articles">Votes: {article.votes}</h3>
             </ul>
           </div>
         );
@@ -35,25 +38,31 @@ export const ArticleList = props => {
       <div className="articleCardsCon">
         {props.articles.slice(1, 20).map(article => {
           return (
-            <div className="articleCard" key={article.article_id}>
-              <div className="articleL">
-                <Link className="articles" to={`${article.article_id}`}>
-                  <h3> {article.title}</h3>
-                  <hr className="cardHr" />
-                  {article.author}
-                </Link>
-                {/* {props.created_at && ( */}
-                <p>Date posted: {article.created_at.slice(0, 10)}</p>
-                {/* )} */}
-                {/* {props.comment_count && ( */}
-                <p>Comment count: {article.comment_count}</p>
-                {/* )} */}
-                {/* {props.votes &&  ( */}
-                <p>Votes: {article.votes}</p>
-                {/* )} */}
-                {/* <hr className="articleList" /> */}
+            <Link className="articleCard" to={`${article.article_id}`}>
+              <div key={article.article_id}>
+                <div className="articleL">
+                  <Link to={`${article.article_id}`} className="cardLink">
+                    <h3 className="articles"> {article.title}</h3>
+                    <hr className="cardHr" />
+                    <p className="articles">{article.author}</p>
+                  </Link>
+                  {/* {props.created_at && ( */}
+                  <p className="articles">
+                    Date posted: {article.created_at.slice(0, 10)}
+                  </p>
+                  {/* )} */}
+                  {/* {props.comment_count && ( */}
+                  <p className="articles">
+                    Comment count: {article.comment_count}
+                  </p>
+                  {/* )} */}
+                  {/* {props.votes &&  ( */}
+                  <p className="articles">Votes: {article.votes}</p>
+                  {/* )} */}
+                  {/* <hr className="articleList" /> */}
+                </div>
               </div>
-            </div>
+            </Link>
           );
         })}
       </div>
